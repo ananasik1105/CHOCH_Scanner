72c35f7d8709fbdd8091c231a6a0089441dda679
ananasik1105 <anastasiia11051994@gmail.com>
2026-03-15 10:48:34 +0300

    Update CHOCH_Scanner.py

--- CHOCH_Scanner.py
+++ CHOCH_Scanner.py
@@ -150,6 +150,22 @@ now = datetime.now(tz)
 hour = now.hour
 
 # проверка рабочего времени: с 8:00 до 1:00
+
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CHOCH Scanner running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
 if 8 <= hour or hour <= 1:
     scan()
 else:
